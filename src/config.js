7f14d796b3c611b5b9ab363fc3a9dcc75224d571
Trezy <tre@trezy.com>
2019-12-10 11:02:38 -0600

    refactor: move Firebase credentials into ENV vars

--- src/config.js
+++ src/config.js
@@ -5,12 +5,6 @@ import dotenv from 'dotenv'
 
 
 
-import firebaseCredentials from '../firebase-credentials.json'
-
-
-
-
-
 dotenv.config()
 
 
@@ -25,7 +19,18 @@ module.exports = {
       accessToken: process.env.TOASTR_DISCORD_ACCESS_TOKEN,
     },
     firebase: {
-      credentials: firebaseCredentials,
+      credentials: {
+        auth_provider_x509_cert_url: process.env.TOASTR_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        auth_uri: process.env.TOASTR_FIREBASE_AUTH_URI,
+        client_email: process.env.TOASTR_FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.TOASTR_FIREBASE_CLIENT_ID,
+        client_x509_cert_url: process.env.TOASTR_FIREBASE_CLIENT_X509_CERT_URL,
+        project_id: process.env.TOASTR_FIREBASE_PROJECT_ID,
+        private_key: process.env.TOASTR_FIREBASE_PRIVATE_KEY,
+        private_key_id: process.env.TOASTR_FIREBASE_PRIVATE_KEY_ID,
+        token_uri: process.env.TOASTR_FIREBASE_TOKEN_URI,
+        type: process.env.TOASTR_FIREBASE_TYPE,
+      },
       url: process.env.TOASTR_FIREBASE_DB_URL,
     },
     twitch: {
