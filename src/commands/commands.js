28135689ad42d90ac9e943ea44fbe030f1436830
Trezy <tre@trezy.com>
2019-12-16 14:13:35 -0600

    fix(!commands): add support for more than 25 commands
    Discord limits the number of fields to 25, so we have to split the embed
    if we want to support more than that.

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -19,15 +19,35 @@ export default messageData => {
     defaultPrefix,
   } = messageData
 
+  let embedObjects = Object.values(commands).reduce((accumulator, command, index, array) => {
+    if ((index % 25) === 0) {
+      accumulator.push({
+        color: 'blue',
+        fields: [],
+        title: 'Available Commands',
+      })
+    }
+
+    accumulator[accumulator.length - 1].fields.push({
+      name: `\`${defaultPrefix}${escapeDiscordMarkdown(emoji.emojify(command.name))}\``,
+      value: command.getDescription(messageData),
+    })
+
+    return accumulator
+  }, [])
+
+
+  if (embedObjects.length > 1) {
+    embedObjects = embedObjects.map((embedObject, index, array) => ({
+      ...embedObject,
+      footer: {
+        text: `Page ${index + 1} of ${array.length}`
+      },
+    }))
+  }
+
   return {
-    embed: {
-      color: 'blue',
-      fields: Object.values(commands).map(command => ({
-        name: `\`${defaultPrefix}${escapeDiscordMarkdown(emoji.emojify(command.name))}\``,
-        value: command.getDescription(messageData),
-      })),
-      title: 'Available Commands',
-    },
+    embed: embedObjects,
     say: emoji.emojify(`This is a list of all available commands: ${getCommandList(commands)}.`),
     success: true,
   }
